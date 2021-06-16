22084e9132ef72c060dc968b39917ef9b9e229a6
Hans Oliveira <hansnery@gmail.com>
2021-06-16 20:02:13 -0300

    Add All Events

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :events
-  resources :invitations  #, only: [:create, :destroy, :update]
+  resources :invitations
+  get 'all_events', to: 'events#all_events'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_044342) do
+ActiveRecord::Schema.define(version: 2021_06_16_222949) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_044342) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "private"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
