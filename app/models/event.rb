90d0b57b8b6ea214a6b594018ff46283f4f085a9
Hans Oliveira <hansnery@gmail.com>
2021-06-16 19:18:52 -0300

    Attendees show up on the Events page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   validates :name, :location, :date, presence: true
   has_many :invitations, foreign_key: :attended_event_id, dependent: :destroy
-  has_many :attendees, through: :invitations, source: :user
+  has_many :attendees, through: :invitations
 
   # scope :past, -> { where('date_time < ?', Date.today).order(date_time: :desc) }
   # scope :future, -> { where('date_time > ?', Date.today).order(:date_time) }
