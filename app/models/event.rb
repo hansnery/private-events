13548686c689899dbf2ac5973f33599421785b85
Hans Oliveira <hansnery@gmail.com>
2021-06-29 18:03:47 -0300

    Add desc to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  validates :name, :location, :date, presence: true
+  validates :name, :location, :date, :desc, presence: true
   has_many :invitations, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :invitations
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_222949) do
+ActiveRecord::Schema.define(version: 2021_06_29_202629) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_222949) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "private"
+    t.string "desc"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
