c8aeabfa2e1cc71e2fe4b076b58d6482fd31b076
Hans Oliveira <hansnery@gmail.com>
2021-07-01 16:00:24 -0300

    Can set events as private

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def event_params
-    params.require(:event).permit(:name, :location, :date, :desc, :user_id)
+    params.require(:event).permit(:name, :location, :date, :desc, :private, :user_id)
   end
 end
