e952eec1b5943ebdcada71098feaa1e53c4092e7
Hans Oliveira <hansnery@gmail.com>
2021-07-13 17:26:57 -0300

    Add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+first_names = []
+last_names = []
+users = []
+locations = []
+events = [ "Party", "Birthday", "Soirée", "Movie Night", "Pizza Night", "Pajama Party", "Rally", "Concert", "Conference", "Workshop", "Seminar", "Show", "Panel", "Get Together", "Class", "Zoom Meeting", "Expo", "Award", "Festival" ]
+File.open(File.join(File.dirname(__FILE__), 'names.txt'), "r").each_line do |line|
+  first_names << line.chomp
+end
+File.open(File.join(File.dirname(__FILE__), 'last_names.txt'), "r").each_line do |line|
+  last_names << line.downcase.capitalize.chomp
+end
+File.open(File.join(File.dirname(__FILE__), 'cities.txt'), "r").each_line do |line|
+  locations << line.chomp
+end
+20.times do
+  first_name = first_names.sample
+  last_name = last_names.sample
+  email = "#{first_name.chomp.downcase}@#{first_name.chomp.downcase}.com"
+  user = User.create!(email: email, first_name: first_name, last_name: last_name, admin: false, password: "#{first_name.chomp.downcase}123" )
+  user.save!
+  users << user
+end
+20.times do
+  future_date = DateTime.new(DateTime.now.year + rand(1), DateTime.now.month  + rand(3), DateTime.now.day  + rand(14))
+  host = users.sample
+  event_type = events.sample
+  event = "#{host.first_name.chomp}'s #{event_type}"
+  event = Event.create!(name: event, location: locations.sample, user_id: host.id, private: false, desc: "Please, attend my #{event_type}!", date: future_date)
+  event.save!
+end
